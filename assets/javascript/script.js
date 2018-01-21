9ec6bdc65218e1bab85987d81cdbaeae672d7505
Alex Harris <akharri8@ncsu.edu>
2018-01-20 21:50:07 -0500

    Added Form functionality

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,3 @@
-// To make buttons
-    // Make an array of player names
-    // use a for loop to make buttons from the player names array
-        // Will need to add classes and values or data-names to hook onto that button
 
 playerNames = ["Lebron James", "Kevin Durant", "Chris Bosh",
 "Kobe Bryant", "Steph Curry", "Demar Derozan", "Joel Embiid"];
@@ -9,18 +5,21 @@ playerNames = ["Lebron James", "Kevin Durant", "Chris Bosh",
 const buttonsDiv = $("#buttons");
 const playerImages = $("#images");
 
-for(var i = 0; i < playerNames.length; i++) {
-var button = $("<button>").text(playerNames[i]);
-button.attr("data-name", playerNames[i]);
-button.addClass("playerButton");
-buttonsDiv.append(button);
-}
+function playerButtons() {
+    for(var i = 0; i < playerNames.length; i++) {
+    var button = $("<button>").text(playerNames[i]);
+    button.attr("data-name", playerNames[i]);
+    button.addClass("playerButton");
+    buttonsDiv.append(button);
+    }
+};
+playerButtons();
 
 const apikey = "rE7VKijNZKrTzo6UYIy3sms9lSuQ6c3f";
 
 $(document).on("click", "#clear", removeGifs)
 $(document).on("click", ".playerButton", getGifs);
-$(document).on("click", ".image", //function here)
+$(document).on("click", ".image", animate);
 
 function getGifs() {
     var player = $(this).attr("data-name");
@@ -32,7 +31,9 @@ function getGifs() {
         playerImages.empty();
         console.log(response);
         for(var i = 0; i < 10; i++) {
-            var gif = $("<img>").attr("src", response.data[i].images.fixed_width_still.url);
+            var gif = $("<img>");
+            gif.attr("still", response.data[i].images.fixed_width_still.url);
+            gif.attr("src", response.data[i].images.fixed_width_still.url);
             gif.attr("animated", response.data[i].images.fixed_width.url);
             gif.addClass("image");
             gif.addClass("still");
@@ -41,20 +42,29 @@ function getGifs() {
     });
 }
 
-// function animate() {
-//     if((this).hasClass("still")) {
-//         $(this).removeClass("still");
-//         $(this).addClass("animated");
-//         $(this).attr("src" = )
-//     }
-// }
+function animate() {
+    if($(this).hasClass("still")) {
+        $(this).removeClass("still");
+        $(this).addClass("animated");
+        let animatedGif = $(this).attr("animated");
+        $(this).attr("src", animatedGif);
+    } else if($(this).hasClass("animated")) {
+        $(this).removeClass("animated");
+        $(this).addClass("still");
+        let stillGif = $(this).attr("still");
+        $(this).attr("src", stillGif);
+    }
+}
+
+$("#addPlayer").on("click", function(event) {
+    $("#buttons").empty();
+    event.preventDefault();
+    var newPlayer = $("#newPlayer").val().trim();
+    playerNames.push(newPlayer);
+    playerButtons();
+})
 
 function removeGifs() {
     playerImages.empty();
 }
 
-// Need to get the still image. and set this to the src by default
-// need to get the animated gif, and on click. set that to the src attr
-
-// for the form, need to get the value when submitted.
-// prevent default, so submit works
